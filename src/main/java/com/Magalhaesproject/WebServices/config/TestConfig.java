749242950f8c0136f89bd85199804983019fd66d
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-03-11 11:25:17 -0300

    Update fix

--- src/main/java/com/Magalhaesproject/WebServices/config/TestConfig.java
+++ src/main/java/com/Magalhaesproject/WebServices/config/TestConfig.java
@@ -63,6 +63,7 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
+		
 		Order o1 = new Order(null, Instant.parse("2025-03-10T19:53:07Z"),OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2025-03-11T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2025-03-12T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
--- src/main/java/com/Magalhaesproject/WebServices/repositories/OrderRepository.java
+++ src/main/java/com/Magalhaesproject/WebServices/repositories/OrderRepository.java
@@ -1,7 +1,7 @@
 package com.Magalhaesproject.WebServices.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.Magalhaesproject.WebServices.entities.Category;
+
 import com.Magalhaesproject.WebServices.entities.Order;
 
 
